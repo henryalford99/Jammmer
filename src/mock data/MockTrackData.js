f0708c55e717898eb21f91f2fb8b63a61185c04b
Henry <henrytalford99@gmail.com>
2024-07-15 17:23:40 -0700

    Mock Track Data created

---
+++ src/mock data/MockTrackData.js
@@ -0,0 +1,22 @@
+const trackList = [
+    {
+        name: 'So Long, London',
+        artist: 'Taylor Swift',
+        album: 'THE TORTURED POETS DEPARTMENT',
+        id: 1
+    },
+    {
+        name: 'Red Wine Supernova',
+        artist: 'Chappell Roan',
+        album: 'The Rise and Fall of a Midwest Princess',
+        id: 2
+    },
+    {
+        name: 'Disciples',
+        artist: 'Tame Impala',
+        album: 'Currents',
+        id: 3
+    }
+];
+
+export default trackList;
