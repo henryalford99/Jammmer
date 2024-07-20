f72581a6767579366c384ddd031703370152fb3c
Henry <henrytalford99@gmail.com>
2024-07-19 18:37:44 -0700

    final stylistic adjustments

--- src/sub-components/Playlist.js
+++ src/sub-components/Playlist.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Tracklist from './Tracklist';
+import logo from './spotify_logo.png';
 
 const Playlist = ({ name, tracks, onRemoveTrack, onTitleChange, onSave }) => {
     return (
         <div className="Playlist">
             <input name="playlistName" onChange={onTitleChange} value={name} placeholder="Playlist Title" type="text" />
-            <button onClick={onSave}>SAVE TO SPOTIFY</button>
+            <button onClick={onSave}>
+                <p>SAVE TO SPOTIFY</p>
+                <img src={logo}/>
+            </button>
             <Tracklist tracks={tracks} onRemoveTrack={onRemoveTrack} isRemovable={true}/>
         </div>
     )
